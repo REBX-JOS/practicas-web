b2ef2f995d856aa5300683eb7a8b9af5c01a377c
REBX <jos2004ty@gmail.com>
2026-02-17 23:29:19 -0600

    Cambiamos como es que interactuan las rutas en app.route.ts, que estaban
    Cambiamos las rutas de app.route.ts que tenian mal las comillas
    Agregamos body para product-card.html y ver un preview
    Modificamos product-card.ts para que pudiese recibir un objeto producto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { Catalogo } from '../components/catalogo/catalogo';
 
 export const routes: Routes = [
-    { path: ", component: CatalogoComponent"},
-    { path: '**', redirectTo: ""},
+    { path: '', component: Catalogo },
+    { path: '**', redirectTo: ''},
 ];
--- src/components/producto-card/producto-card.ts
+++ src/components/producto-card/producto-card.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../models/producto.model';
 
 @Component({
   selector: 'app-producto-card',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
 })
 
 export class ProductoCard {
-
+  @Input() product!: Product;
 }
